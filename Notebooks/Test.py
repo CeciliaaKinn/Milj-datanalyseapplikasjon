7213e01983cbc59da68a8fa88df07d6b247a0957
emeliahult <emeliah@stud.ntnu.no>
2025-02-27 14:58:27 +0100

    Requests

--- Notebooks/Test.py
+++ Notebooks/Test.py
@@ -1,6 +1,23 @@
-print("Utviklingsmiljøet er klart!")
+# API
 
-# Laste ned API
+import requests
+import json
+
+
+url = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=0"
+
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print(response.json())  # Hvis API-et returnerer JSON-data
+else:
+    print(f"Feil: {response.status_code}")
+
+
+
+# Bruke pandas data frame for å lage en tabell med data 
+
 
 
 
